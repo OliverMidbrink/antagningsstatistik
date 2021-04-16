6b03905d0125ed9cda5c666b476c133c4b52d1ec
OliverMidbrink <oliver.midbrink@hotmail.com>
2021-04-16 02:49:07 +0200

    Made a simple start for a ui

--- react-spa/src/App.js
+++ react-spa/src/App.js
@@ -1,22 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import InputBase from '@material-ui/core/InputBase';
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
+import Input from '@material-ui/core/Input';
+import React from "react";
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    width: 400,
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  },
+  divider: {
+    height: 28,
+    margin: 4,
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
+  const [value, setValue] = React.useState("");
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert('A name was submitted: ' + value);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Sök en utbildning och se dina chanser att komma in!</p>
+        <form className={classes.root} onSubmit={handleSubmit}>
+          <Paper className={classes.root}>
+            <InputBase
+              className={classes.input}
+              placeholder="Läkarprogrammet Karolinska"
+              value={value}
+              onChange={handleChange}
+              inputProps={{ 'aria-label': 'sök efter utbildningar' }}
+            />
+            <IconButton type="submit" className={classes.iconButton} aria-label="search">
+              <SearchIcon />
+            </IconButton>
+          </Paper>
+        </form>
       </header>
     </div>
   );
