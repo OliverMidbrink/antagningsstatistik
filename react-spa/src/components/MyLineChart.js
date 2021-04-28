83a38632048bec1cb2a1fe84a3d4b34cd34c443b
OliverMidbrink <oliver.midbrink@hotmail.com>
2021-04-28 08:59:10 +0200

    Made chart more general purpose, added input for users grades

--- react-spa/src/components/MyLineChart.js
+++ react-spa/src/components/MyLineChart.js
@@ -9,50 +9,85 @@ import {
   Legend
 } from "recharts";
 
-function processData(inputData) {
-  var outputData = [];
-  console.log(inputData);
+function processData(inputData, displayFilter, userHP, userBI, userBII) {
+  try {
+    var outputData = [];
+    console.log(inputData);
 
-  if (inputData.length > 0) {
+    var startMyLinesLaid = false;
 
-    var year;
-    var endYear = new Date().getFullYear() + 1;
-    for (year = 2009; year < endYear; year++) {
+    if (inputData.length > 0) {
 
-      var term;
-      for (term = 0; term < 2; term++){
-        var item = "not found";
+      var year;
+      var endYear = new Date().getFullYear() + 1;
+      for (year = 2009; year < endYear; year++) {
 
-        if(term === 0){
-          // VT
-          item = inputData[2].find(element => element[0] === year);
-        } else {
-          // HT
-          item = inputData[1].find(element => element[0] === year);
+        var term;
+        for (term = 0; term < 2; term++){
+          var item = "not found";
+
+          if(term === 0){
+            // VT
+            item = inputData[2].find(element => element[0] === year);
+          } else {
+            // HT
+            item = inputData[1].find(element => element[0] === year);
+          }
+
+          //console.log(item);
+
+          if(item === "not found" || item === undefined) {
+            continue;
+          }
+
+          var data_points = {
+            name:item[1] + " " + item[0],
+          };
+    
+          if(parseFloat(item[2]) !== 'NaN' && displayFilter.includes("BI")) data_points["BI"] = parseFloat(item[2]);
+          if(parseFloat(item[3]) !== 'NaN' && displayFilter.includes("BII")) data_points["BII"] = parseFloat(item[3]);
+          if(parseFloat(item[4]) !== 'NaN' && displayFilter.includes("HP")) data_points["HP"] = parseFloat(item[4]);
+
+          // Add user values if they are valid
+          if(startMyLinesLaid === false) {
+            startMyLinesLaid = true;
+            if(userHP !== undefined) {
+              console.log(userHP.replace(',', '.'));
+              data_points["Ditt HP"] = parseFloat(userHP.replace(',', '.'));
+            }
+            if(userBI !== undefined) {
+              data_points["Ditt BI"] = parseFloat(userBI.replace(',', '.'));
+            }
+            if(userBII !== undefined) {
+              data_points["Ditt BII"] = parseFloat(userBII.replace(',', '.'));
+            }
+          }
+          
+          console.log(data_points);
+          outputData.push(data_points);
         }
-
-        console.log(item);
-
-        if(item === "not found" || item === undefined) {
-          continue;
-        }
-
-        var data_points = {
-          name:item[1] + " " + item[0],
-        };
-  
-        if(parseFloat(item[2]) !== 'NaN') data_points["BI"] = parseFloat(item[2]);
-        if(parseFloat(item[3]) !== 'NaN') data_points["BII"] = parseFloat(item[3]);
-        if(parseFloat(item[4]) !== 'NaN') data_points["HP"] = parseFloat(item[4]);
-  
-        outputData.push(data_points);
       }
-    }
+      
+      // Add user values if they are valid
+      if(userHP !== undefined) {
+        outputData[outputData.length - 1]["Ditt HP"] = parseFloat(userHP.replace(',', '.'));
+      }
+      if(userBI !== undefined) {
+        outputData[outputData.length - 1]["Ditt BI"] = parseFloat(userBI.replace(',', '.'));
+      }
+      if(userBII !== undefined) {
+        outputData[outputData.length - 1]["Ditt BII"] = parseFloat(userBII.replace(',', '.'));
+      }
 
-    return outputData;
-  } else {
+      return outputData;
+    } else {
+      
+    }
+  } catch (error) {
+    console.log(error);
     return inputData;
   }
+  
 }
 
 
@@ -63,7 +98,7 @@ export default function MyLineChart(props) {
     <LineChart
       width={props.width}
       height={320}
-      data={processData(props.programData)}
+      data={processData(props.programData, props.displayFilter, props.userHP, props.userBI, props.userBII)}
       margin={{
         top: 15,
         right: 15,
@@ -72,20 +107,36 @@ export default function MyLineChart(props) {
       }}
     >
       <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" interval={2} angle={40} height={70} dx={23} dy={20}/>
+      <XAxis dataKey="name" interval={'auto'} angle={40} height={70} dx={23} dy={20}/>
       <YAxis yAxisId="left" domain={['auto', 'auto']}/>
       <YAxis yAxisId="right" orientation="right" domain={['dataMin-0.2', 'auto']} />
       <Tooltip />
       <Legend />
-      <Line
-        yAxisId="left"
-        type="monotone"
-        dataKey="BI"
-        stroke="#8884d8"
-        activeDot={{ r: 8 }}
-      />
-      <Line yAxisId="left" type="monotone" dataKey="BII" stroke="#82ca9d" />
-      <Line yAxisId="right" type="monotone" dataKey="HP" stroke="orange" />
+      {props.displayFilter.includes("BI") &&
+        <Line
+          yAxisId="left"
+          type="monotone"
+          dataKey="BI"
+          stroke="#8884d8"
+          activeDot={{ r: 8 }}
+        />
+      }
+      {props.displayFilter.includes("BII") &&
+        <Line yAxisId="left" type="monotone" dataKey="BII" stroke="#82ca9d" />
+      }
+      
+      {props.displayFilter.includes("HP") &&
+        <Line yAxisId="left" type="monotone" dataKey="HP" stroke="orange" />
+      }
+      {props.displayFilter.includes("HP") &&
+        <Line connectNulls yAxisId="left" type="monotone" dataKey="Ditt HP" stroke="red" />
+      }
+      {props.displayFilter.includes("BI") &&
+        <Line connectNulls yAxisId="left" type="monotone" dataKey="Ditt BI" stroke="red" />
+      }
+      {props.displayFilter.includes("BII") &&
+        <Line connectNulls yAxisId="left" type="monotone" dataKey="Ditt BII" stroke="#d42600" />
+      }
     </LineChart>
   );
 }
