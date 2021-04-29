6f57b097c6d2db85e3a7c1bc51d2a81f8bcc171a
OliverMidbrink <oliver.midbrink@hotmail.com>
2021-04-29 23:14:07 +0200

    changed port to 80

--- antagningsstatistik_webscraper.py
+++ antagningsstatistik_webscraper.py
@@ -208,5 +208,5 @@ api.add_resource(Query, '/query') # Api server on port 5002
 api.add_resource(Program_data, '/program_data')
 
 if __name__ == '__main__':
-    app.run(port='5002')
+    app.run(port='80')
 
