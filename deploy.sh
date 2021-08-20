53d1a856a4da061cf32cb846e89a2757572ac937
Oliver Midbrink <oliver.midbrink@hotmail.com>
2021-08-20 15:25:36 +0200

    d

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
-python3 antagningstatistik_webscraper.py & 
-serve -p 80 react-spa/build
+npm install pm2 -g
+sudo apt install python3.8 -y
+pm2 --name MinachanserServer start "antagningstatistik_webscraper.py" "serve -p 80 react-spa/build"
 
