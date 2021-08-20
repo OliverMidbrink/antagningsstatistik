1dbc452a2b476d6c1b8723d77bd06cb6cedefc43
Oliver Midbrink <oliver.midbrink@hotmail.com>
2021-08-20 15:31:34 +0200

    d

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 npm install pm2 -g
 npm i serve -g
 sudo apt install python3.8 -y
-pm2 --name MinachanserServer start "antagningstatistik_webscraper.py" "serve -p 80 react-spa/build"
+pm2 --name MinachanserServer start "antagningsstatistik_webscraper.py" "serve -p 80 react-spa/build"
 
